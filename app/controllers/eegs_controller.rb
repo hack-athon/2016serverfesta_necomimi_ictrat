eb592be304ff6169d6640e05a62c3274896519f3
hotuta <8985703@gmail.com>
2016-03-26 12:16:48 +0900

    destroy追加

--- app/controllers/eegs_controller.rb
+++ app/controllers/eegs_controller.rb
@@ -3,4 +3,9 @@ class EegsController < ApplicationController
     @title = "Model Example"
     @datas = Eeg.all
   end
+  def destroy
+    @datas = Eeg.all
+    @datas.destroy
+    redirect_to eegs_index_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get 'auth/signout' => 'sessions#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  delete '/eegs/index' => 'eegs#destroy', as: 'destroy_eegs'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
